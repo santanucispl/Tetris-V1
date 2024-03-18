2634df628454ab99114d80b1c31526bbc6c27992
santanucispl <63183875+santanucispl@users.noreply.github.com>
2024-03-18 17:51:07 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "shan-terraform-state-file" # Replace with your actual S3 bucket name
     key    = "Jenkins2/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 }
